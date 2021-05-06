5ccedd7ce52af1d39e1ddbfeb1bef23d63d7a256
skabi <skabillium@gmail.com>
2021-05-06 12:08:40 +0300

    Add type support to config service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './config/configuration';
+import { DatabaseConfig } from './config/types';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        ...(await configService.get('DATABASE')),
-        type: 'postgres',
-        entities: [],
-        synchronize: (await configService.get('ENV')) === 'development',
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const databaseConfig = configService.get<DatabaseConfig>('DATABASE');
+
+        return {
+          host: databaseConfig.host,
+          database: databaseConfig.name,
+          username: databaseConfig.user,
+          password: databaseConfig.password,
+          port: databaseConfig.port,
+          type: 'postgres',
+          entities: [],
+          synchronize: configService.get('ENV') === 'development',
+        };
+      },
       inject: [ConfigService],
     }),
   ],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,6 +4,7 @@ export default () => ({
     host: process.env.DATABASE_HOST,
     port: parseInt(process.env.DATABASE_PORT, 10),
     user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
     name: process.env.DATABASE_NAME,
   },
   FRONTEND: {
