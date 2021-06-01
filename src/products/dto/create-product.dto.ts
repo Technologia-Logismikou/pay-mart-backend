2f471c1f05b7979f8508cd5da66f054e57d68ad3
VoTheGreat <43538351+VoTheGreat@users.noreply.github.com>
2021-06-02 00:20:19 +0300

    added controller and .service

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,10 +1,23 @@
 // Only list the properties that need to be sent by the seller
+import {ApiProperty} from '@nestjs/swagger';
+
 export class CreateProductDto {
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   category: string;
+
+  @ApiProperty()
   description: string;
+
+  @ApiProperty()
   price: number;
+
+  @ApiProperty()
   photos: string[];
+
+  
   public: boolean;
   url: string;
   slug: string;
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,11 +2,35 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './products.entity';
-
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+ 
 @Injectable()
 export class ProductsService {
   constructor(
     @InjectRepository(Product)
     private readonly productsRepository: Repository<Product>,
   ) {}
+
+  create(createProductDto: CreateProductDto) {
+    const product = this.productsRepository.create(createProductDto);
+    return this.productsRepository.save(product);
+  }
+
+  findAll() {
+    return this.productsRepository.find();
+  }
+
+  findOne(id: string) {
+    return this.productsRepository.findOne(id);
+  }
+
+  update(id: string, updateProductDto: UpdateProductDto) {
+    return this.productsRepository.update(id, updateProductDto);
+  }
+
+  remove(id: string) {
+    return this.productsRepository.delete(id);
+  }
+
 }
