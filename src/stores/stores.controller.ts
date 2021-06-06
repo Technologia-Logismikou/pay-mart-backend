5fd5986d5264b281dfec8f8c42dfaac61549edd2
skabi <skabillium@gmail.com>
2021-06-06 18:33:46 +0300

    Add necessary routes and service funtions to store module
    - GET /stores
    - GET /stores/{id}
    - Update /stores/{id}

---
+++ src/stores/stores.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Put, Body, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { StoresService } from './stores.service';
+import { UpdateStoreDto } from './dto/update-store.dto';
+
+@ApiTags('Stores')
+@Controller('stores')
+export class StoresController {
+  constructor(private readonly storesService: StoresService) {}
+
+  @Get()
+  findAll() {
+    return this.storesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.storesService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateStoreDto: UpdateStoreDto) {
+    return this.storesService.update(id, updateStoreDto);
+  }
+}
